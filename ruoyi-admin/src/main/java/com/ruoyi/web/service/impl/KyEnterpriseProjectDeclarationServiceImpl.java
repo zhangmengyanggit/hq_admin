c1e26b7d529dcf84986fe2a4b50f4cbe7e4752c5
张梦阳 <344534313@qq.com>
2023-05-09 15:20:37 +0800

    首次提交

--- ruoyi-admin/src/main/java/com/ruoyi/web/service/impl/KyEnterpriseProjectDeclarationServiceImpl.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/service/impl/KyEnterpriseProjectDeclarationServiceImpl.java
@@ -19,41 +19,46 @@ import javax.management.RuntimeOperationsException;
 
 /**
  * 企业申请项目Service业务层处理
- * 
+ *
  * @author ruoyi
  * @date 2023-03-15
  */
 @Service
-public class KyEnterpriseProjectDeclarationServiceImpl implements IKyEnterpriseProjectDeclarationService
-{
+public class KyEnterpriseProjectDeclarationServiceImpl implements IKyEnterpriseProjectDeclarationService {
     @Autowired
     private KyEnterpriseProjectDeclarationMapper kyEnterpriseProjectDeclarationMapper;
 
     /**
      * 查询企业申请项目
-     * 
+     *
      * @param id 企业申请项目主键
      * @return 企业申请项目
      */
     @Override
-    public KyEnterpriseProjectDeclarationVo selectKyEnterpriseProjectDeclarationById(Long id)
-    {
+    public KyEnterpriseProjectDeclarationVo selectKyEnterpriseProjectDeclarationById(Long id) {
         return kyEnterpriseProjectDeclarationMapper.selectKyEnterpriseProjectDeclarationById(id);
     }
 
     /**
      * 查询企业申请项目列表
-     * 
+     *
      * @param kyEnterpriseProjectDeclaration 企业申请项目
      * @return 企业申请项目
      */
     @Override
-    public List<KyEnterpriseProjectDeclarationVo> selectKyEnterpriseProjectDeclarationList(KyEnterpriseProjectDeclarationVo kyEnterpriseProjectDeclaration)
-    {
-        List<KyEnterpriseProjectDeclarationVo>  kyEnterpriseProjectDeclarationVos=kyEnterpriseProjectDeclarationMapper.selectKyEnterpriseProjectDeclarationList(kyEnterpriseProjectDeclaration);
-        kyEnterpriseProjectDeclarationVos.stream().forEach(e->{
-            e.setBusinessLicenseUrl(RuoYiConfig.getSeverWebUrl()+e.getBusinessLicenseUrl());
-            e.setIdentityCardUrl(RuoYiConfig.getSeverWebUrl()+e.getIdentityCardUrl());
+    public List<KyEnterpriseProjectDeclarationVo> selectKyEnterpriseProjectDeclarationList(KyEnterpriseProjectDeclarationVo kyEnterpriseProjectDeclaration) {
+        List<KyEnterpriseProjectDeclarationVo> kyEnterpriseProjectDeclarationVos = kyEnterpriseProjectDeclarationMapper.selectKyEnterpriseProjectDeclarationList(kyEnterpriseProjectDeclaration);
+        kyEnterpriseProjectDeclarationVos.stream().forEach(e -> {
+            if (!RuoYiConfig.isDemoEnabled()) {
+                //本地
+                e.setBusinessLicenseUrl(RuoYiConfig.getSeverWebUrl() + e.getBusinessLicenseUrl());
+                e.setIdentityCardUrl(RuoYiConfig.getSeverWebUrl() + e.getIdentityCardUrl());
+            } else {
+                //政务内网
+                e.setBusinessLicenseUrl(RuoYiConfig.getSeverWebUrl() + "/prod-api" + e.getBusinessLicenseUrl());
+                e.setIdentityCardUrl(RuoYiConfig.getSeverWebUrl() + "/prod-api" + e.getIdentityCardUrl());
+            }
+
         });
 
         return kyEnterpriseProjectDeclarationVos;
@@ -61,31 +66,27 @@ public class KyEnterpriseProjectDeclarationServiceImpl implements IKyEnterpriseP
 
     /**
      * 新增企业申请项目
-     * 
+     *
      * @param kyEnterpriseProjectDeclaration 企业申请项目
      * @return 结果
      */
     @Override
     @Transactional
-    public int insertKyEnterpriseProjectDeclaration(KyEnterpriseProjectDeclaration kyEnterpriseProjectDeclaration)
-    {
+    public int insertKyEnterpriseProjectDeclaration(KyEnterpriseProjectDeclaration kyEnterpriseProjectDeclaration) {
         return kyEnterpriseProjectDeclarationMapper.insertKyEnterpriseProjectDeclaration(kyEnterpriseProjectDeclaration);
     }
 
     /**
      * 修改企业申请项目
-     * 
+     *
      * @param kyEnterpriseProjectDeclaration 企业申请项目
      * @return 结果
      */
     @Override
     @Transactional
-    public int updateKyEnterpriseProjectDeclaration(KyEnterpriseProjectDeclaration kyEnterpriseProjectDeclaration)
-    {
+    public int updateKyEnterpriseProjectDeclaration(KyEnterpriseProjectDeclaration kyEnterpriseProjectDeclaration) {
         return kyEnterpriseProjectDeclarationMapper.updateKyEnterpriseProjectDeclaration(kyEnterpriseProjectDeclaration);
     }
-
-
 
 
     @Override
@@ -95,10 +96,10 @@ public class KyEnterpriseProjectDeclarationServiceImpl implements IKyEnterpriseP
 
     @Override
     @Transactional
-    public int auditKyEnterpriseProjectDeclarationByIds(Long[] ids,Integer status) {
-        Map<String,Object>  paramsMap=new HashMap<String,Object>();
-        paramsMap.put("ids",ids);
-        paramsMap.put("auditStatus",status);
+    public int auditKyEnterpriseProjectDeclarationByIds(Long[] ids, Integer status) {
+        Map<String, Object> paramsMap = new HashMap<String, Object>();
+        paramsMap.put("ids", ids);
+        paramsMap.put("auditStatus", status);
         return kyEnterpriseProjectDeclarationMapper.updateByIds(paramsMap);
     }
 
@@ -109,10 +110,10 @@ public class KyEnterpriseProjectDeclarationServiceImpl implements IKyEnterpriseP
 
     @Override
     @Transactional
-    public int acceptKyEnterpriseProjectDeclarationByIds(Long[] ids,Integer status) {
-        Map<String,Object>  paramsMap=new HashMap<String,Object>();
-        paramsMap.put("ids",ids);
-        paramsMap.put("auditStatus",status);
+    public int acceptKyEnterpriseProjectDeclarationByIds(Long[] ids, Integer status) {
+        Map<String, Object> paramsMap = new HashMap<String, Object>();
+        paramsMap.put("ids", ids);
+        paramsMap.put("auditStatus", status);
         return kyEnterpriseProjectDeclarationMapper.updateByIds(paramsMap);
     }
 
